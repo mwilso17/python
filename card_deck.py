e0901cca5ee9e967b2b3f5a7efea22e12fba1f4a
mwilso17 <michael.b.wilson2@gmail.com>
2024-12-15 06:39:59 -0500

    Added standard deck of playing cards (52 cards)

---
+++ card_deck.py
@@ -0,0 +1,4 @@
+deck = ["Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 or Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "Kings of Spades",
+         "Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 or Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "Kings of Hearts",
+         "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 or Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "Kings of Clubs",
+         "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 or Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "Kings of Diamonds",]
