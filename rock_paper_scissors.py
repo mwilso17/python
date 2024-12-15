1d6de1a88c87c650a1e5c232c61716f3c144f306
mwilso17 <michael.b.wilson2@gmail.com>
2024-12-15 06:31:32 -0500

    added while loop for repeat plays.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,33 +1,39 @@
 import random
 
-valid_choices = ["rock", "paper", "scissors"]
-
-computer_choice = random.choice(valid_choices)
-
-user_input = input("Please enter your choice 'rock', 'paper', or 'scissors': " )
-user_choice = str(user_input.lower().strip())
 
 wins = 0
 loses = 0
 ties = 0
 
-if (user_choice not in valid_choices):
-  print("You must select either 'rock', 'paper', or 'scissors'.")
-  user_input
-elif (user_choice == computer_choice):
-  print(f"You picked {user_choice}. The computer picked {computer_choice}.")
-  print("This match was a tie!")
-  ties = ties + 1
-  print(f"Wins: {wins}. Loses: {loses}. Ties: {ties}.")
-elif ((user_choice == "rock" and computer_choice == "scissors") or 
-      (user_choice == "paper" and computer_choice == "rock") or 
-      (user_choice == "scissors" and computer_choice == "paper")):
-  print(f"You picked {user_choice}. The computer picked {computer_choice}.")
-  print("You win!")
-  wins = wins + 1
-  print(f"Wins: {wins}. Loses: {loses}. Ties: {ties}.")
-else:
-  print(f"You picked {user_choice}. The computer picked {computer_choice}.")
-  print("You lose!")
-  loses = loses + 1
-  print(f"Wins: {wins}. Loses: {loses}. Ties: {ties}.")
+while True:
+  valid_choices = ["rock", "paper", "scissors"]
+
+  computer_choice = random.choice(valid_choices)
+
+  user_input = input("Please enter your choice 'rock', 'paper', or 'scissors': " )
+  user_choice = str(user_input.lower().strip())
+
+  if (user_choice not in valid_choices):
+    print("You must select either 'rock', 'paper', or 'scissors'.")
+    user_input
+  elif (user_choice == computer_choice):
+    print(f"You picked {user_choice}. The computer picked {computer_choice}.")
+    print("This match was a tie!")
+    ties = ties + 1
+    print(f"Wins: {wins}. Loses: {loses}. Ties: {ties}.")
+  elif ((user_choice == "rock" and computer_choice == "scissors") or 
+        (user_choice == "paper" and computer_choice == "rock") or 
+        (user_choice == "scissors" and computer_choice == "paper")):
+    print(f"You picked {user_choice}. The computer picked {computer_choice}.")
+    print("You win!")
+    wins = wins + 1
+    print(f"Wins: {wins}. Loses: {loses}. Ties: {ties}.")
+  else:
+    print(f"You picked {user_choice}. The computer picked {computer_choice}.")
+    print("You lose!")
+    loses = loses + 1
+    print(f"Wins: {wins}. Loses: {loses}. Ties: {ties}.")
+
+  play_again = input("Play again? (y/n): ")
+  if play_again.lower().strip() != "y":
+    break
