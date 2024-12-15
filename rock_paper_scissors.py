a3fd83a3ddc85845c9acbc2662cf3bac784c95e7
mwilso17 <michael.b.wilson2@gmail.com>
2024-12-15 06:08:38 -0500

    added computer choice and further validation

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,10 +1,15 @@
+import random
+
 valid_choices = ["rock", "paper", "scissors"]
 
+computer_choice = random.choice(valid_choices)
+
 user_input = input("Please enter your choice 'rock', 'paper', or 'scissors': " )
-user_input_lower = str(user_input.lower())
+user_input_lower = str(user_input.lower().strip())
 
 if (user_input_lower not in valid_choices):
   print("You must select either 'rock', 'paper', or 'scissors'.")
   user_input
 else:
   print(f'Your choice is {user_input_lower}.')
+  print(f'the computer picked {computer_choice}')
