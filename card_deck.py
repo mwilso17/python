3367af6c06c7739d6cf787d6128e304c8aca2bcf
mwilso17 <michael.b.wilson2@gmail.com>
2024-12-23 06:48:35 -0500

    added face and numeric. started guess for face and numeric

--- card_deck.py
+++ card_deck.py
@@ -1,9 +1,9 @@
 import random
 
-deck = {"Ace of Spades": ["black", "spade", "numeric"], "2 of Spades": ["black", "spade", "numeric"], "3 of Spades": ["black", "spade", "numeric"], "4 of Spades": ["black", "spade", "numeric"], "5 of Spades": ["black", "spade", "numeric"], "6 of Spades": ["black", "spade", "numeric"], "7 of Spades": ["black", "spade", "numeric"], "8 of Spades": ["black", "spade", "numeric"], "9 or Spades": ["black", "spade", "numeric"], "10 of Spades": ["black", "spade", "numeric"], "Jack of Spades": ["black", "spade", "face"], "Queen of Spades": ["black", "spade", "face"], "Kings of Spades": ["black", "spade", "face"],
-         "Ace of Hearts": ["red", "heart", "numeric"], "2 of Hearts": ["red", "heart", "numeric"], "3 of Hearts": ["red", "heart", "numeric"], "4 of Hearts": ["red", "heart", "numeric"], "5 of Hearts": ["red", "heart", "numeric"], "6 of Hearts": ["red", "heart", "numeric"], "7 of Hearts": ["red", "heart", "numeric"], "8 of Hearts": ["red", "heart", "numeric"], "9 or Hearts": ["red", "heart", "numeric"], "10 of Hearts": ["red", "heart", "numeric"], "Jack of Hearts": ["red", "heart", "face"], "Queen of Hearts": ["red", "heart", "face"], "Kings of Hearts": ["red", "heart", "face"],
-         "Ace of Clubs": ["black", "club", "numeric"], "2 of Clubs": ["black", "club", "numeric"], "3 of Clubs": ["black", "club", "numeric"], "4 of Clubs": ["black", "club", "numeric"], "5 of Clubs": ["black", "club", "numeric"], "6 of Clubs": ["black", "club", "numeric"], "7 of Clubs": ["black", "club", "numeric"], "8 of Clubs": ["black", "club", "numeric"], "9 or Clubs": ["black", "club", "numeric"], "10 of Clubs": ["black", "club", "numeric"], "Jack of Clubs": ["black", "club", "face"], "Queen of Clubs": ["black", "club", "face"], "Kings of Clubs": ["black", "club", "face"],
-         "Ace of Diamonds": ["red", "diamond", "numeric"], "2 of Diamonds": ["red", "diamond", "numeric"], "3 of Diamonds": ["red", "diamond", "numeric"], "4 of Diamonds": ["red", "diamond", "numeric"], "5 of Diamonds": ["red", "diamond", "numeric"], "6 of Diamonds": ["red", "diamond", "numeric"], "7 of Diamonds": ["red", "diamond", "numeric"], "8 of Diamonds": ["red", "diamond", "numeric"], "9 or Diamonds": ["red", "diamond", "numeric"], "10 of Diamonds": ["red", "diamond", "numeric"], "Jack of Diamonds": ["red", "diamond", "face"], "Queen of Diamonds": ["red", "diamond", "face"], "Kings of Diamonds": ["red", "diamond", "face"]}
+deck = {"Ace of Spades": ["black", "spade", "numeric", 1], "2 of Spades": ["black", "spade", "numeric", 2], "3 of Spades": ["black", "spade", "numeric", 3], "4 of Spades": ["black", "spade", "numeric", 4], "5 of Spades": ["black", "spade", "numeric", 5], "6 of Spades": ["black", "spade", "numeric", 6], "7 of Spades": ["black", "spade", "numeric", 7], "8 of Spades": ["black", "spade", "numeric", 8], "9 or Spades": ["black", "spade", "numeric", 9], "10 of Spades": ["black", "spade", "numeric", 10], "Jack of Spades": ["black", "spade", "face"], "Queen of Spades": ["black", "spade", "face"], "Kings of Spades": ["black", "spade", "face"],
+         "Ace of Hearts": ["red", "heart", "numeric", 1], "2 of Hearts": ["red", "heart", "numeric", 2], "3 of Hearts": ["red", "heart", "numeric", 3], "4 of Hearts": ["red", "heart", "numeric", 4], "5 of Hearts": ["red", "heart", "numeric", 5], "6 of Hearts": ["red", "heart", "numeric", 6], "7 of Hearts": ["red", "heart", "numeric", 7], "8 of Hearts": ["red", "heart", "numeric", 8], "9 or Hearts": ["red", "heart", "numeric", 9], "10 of Hearts": ["red", "heart", "numeric", 10], "Jack of Hearts": ["red", "heart", "face"], "Queen of Hearts": ["red", "heart", "face"], "Kings of Hearts": ["red", "heart", "face"],
+         "Ace of Clubs": ["black", "club", "numeric", 1], "2 of Clubs": ["black", "club", "numeric", 2], "3 of Clubs": ["black", "club", "numeric", 3], "4 of Clubs": ["black", "club", "numeric", 4], "5 of Clubs": ["black", "club", "numeric", 5], "6 of Clubs": ["black", "club", "numeric", 6], "7 of Clubs": ["black", "club", "numeric", 7], "8 of Clubs": ["black", "club", "numeric", 8], "9 or Clubs": ["black", "club", "numeric", 9], "10 of Clubs": ["black", "club", "numeric", 10], "Jack of Clubs": ["black", "club", "face"], "Queen of Clubs": ["black", "club", "face"], "Kings of Clubs": ["black", "club", "face"],
+         "Ace of Diamonds": ["red", "diamond", "numeric", 1], "2 of Diamonds": ["red", "diamond", "numeric", 2], "3 of Diamonds": ["red", "diamond", "numeric", 3], "4 of Diamonds": ["red", "diamond", "numeric", 4], "5 of Diamonds": ["red", "diamond", "numeric", 5], "6 of Diamonds": ["red", "diamond", "numeric", 6], "7 of Diamonds": ["red", "diamond", "numeric", 7], "8 of Diamonds": ["red", "diamond", "numeric", 8], "9 or Diamonds": ["red", "diamond", "numeric", 9], "10 of Diamonds": ["red", "diamond", "numeric", 10], "Jack of Diamonds": ["red", "diamond", "face"], "Queen of Diamonds": ["red", "diamond", "face"], "Kings of Diamonds": ["red", "diamond", "face"]}
 
 colors = ["red", "black"]
 suits = []
@@ -45,18 +45,14 @@ else:
       if suit[1] == suit_guess:
         del deck[card]
 
-print(deck)
-
-
 third_question = input(f"Is your card a face card? (y/n): ")
 
 if third_question == "y":
-  for card, type in list(deck.items()):
-    if type[2] != "face":
+  for card, numeric_or_face in list(deck.items()):
+    if numeric_or_face[2] != "face":
       del deck[card]
-else:
-  for card, type in list(deck.items()):
-      if type[2] != "numeric":
-        del deck[card]
 
-print(deck)
+  card_list = list(deck.keys())
+  computer_guess = random.choice(card_list)
+  print(f"Is your card the {computer_guess}?")
+                       
