5245bbca5d642749179f8bea9ca0c829e8751c71
mwilso17 <michael.b.wilson2@gmail.com>
2024-12-31 06:01:04 -0500

    base code for pizza restaurant. Added TODO lists.

--- pizza_restaurant.py
+++ pizza_restaurant.py
@@ -1,26 +1,61 @@
 # The goal of this program is to simulate a pizza restaurant taking an order from a customer and giving them a price based on their selections.
 # The program takes an order via input statements, notifies the customer if any items are unavailable, prints the request, and gives them a price for the order.
 
-store_sizes = ['small', 'medium', 'large', 'x-large']
+#TODO: store sales seperately. Calculate sales at end of day.
+#TODO: calculate costs vs sales.
+#TODO: Cash vs credit sales
+#TODO: Delivery system vs store pick up.
+class Pizza:
+  def __init__(self, size="", toppings=None):
+    self.size = size
+    self.toppings = toppings if toppings else []
+    self.price = self.calculate_price()
 
-store_crusts = ['thin', 'hand tossed', 'stuffed crust']
+  def calculate_price(self):
+    #TODO: Add crust types (thin, hand tossed, new york, stuffed crust, and price accordingly)
+    #TODO: Adjust prices for toppings based on size of pizza (ie: small = 1, medium = 1.25, large = 1.75)
+    #TODO: Add premium pizza toppings with own price list (ie: bacon, chicken, stake, and shittake mushrooms cost double the normal topping price)
+    #TODO: Add taxes! Cause taxes are a thing. 
+    #TODO: Add tipping system, cause why not.
+    base_price = {"small": 5.99, "medium": 9.99, "large": 11.99}
+    topping_price = 1
+    return base_price[self.size] + (topping_price * len(self.toppings))
+  
+  def __str__(self):
+    return f"{self.size} pizza topped with {', '.join(self.toppings)} - total: ${self.price}!"
+  
+def order_food():
+  size = input("What size pizza do you want? (small, medium, large): ")
+  toppings = []
+  while True:
+    #TODO: If someone types in multiple toppings seperated as a comma, program only tallys for 1 topping. Need to fix.
+    #TODO: Program repeats the topping input line after every press of 'return'. Need it to be less repetitive.
+    #TODO: Program currently allows anything to be input in topping (ie: you can put shoes as a topping). Need to have users only select from a list of approved toppings.
+    #TODO: Extra thoughts: add wings, drinks, and bread items to menu.
+    #TODO: Extra thoughts: Add 'out of stock' toppings and items and let customer know if a requested item is out of stock. 
+    topping = input("What toppings would you like? When finished, type 'done'. :") 
+    if topping.lower() == "done":
+      break
+    toppings.append(topping)
+  
+  return Pizza(size, toppings)
 
-store_toppings = ['pepperoni', 'bacon', 'sausage', 'ham', 'pineapple', 'mushrooms', 'onions', 'green peppers']
 
-tax = .08
+if __name__ == "__main__":
+  print("Welcome to your local pizza restaurant!")
+  order = []
 
+  while True:
+    pizza = order_food()
+    order.append(pizza)
+    order_more = input("Would you like to order more? (y/n): ")
+    if order_more.lower() == 'n':
+      break
 
-customer_order = input('Thank you for choosing the pizza restaurant! What size pizza do you want? (s/m/l/xl): ')
+  print("\nYou ordered: ")
+  total_price = 0
+  for pizza in order:
+    print(pizza)
+    total_price += pizza.price
 
-if customer_order == 's':
-  total = 6
-  print(f'Ok, one small pizza! That will be ${total}')
-if customer_order == 'm':
-  total = 8
-  print(f'Ok, one small pizza! That will be ${total}')
-if customer_order == 'l':
-  total = 10
-  print(f'Ok, one small pizza! That will be ${total}')
-if customer_order == 'xl':
-  total = 12
-  print(f'Ok, one small pizza! That will be ${total}')
+  print(f"\nYour total price is: ${total_price}.")
