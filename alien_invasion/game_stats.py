b2ecc2b2242d408f1107bf653e159d7c6c33a2f7
mwilso17 <michael.b.wilson2@gmail.com>
2025-03-23 19:00:51 -0400

    added high score.

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -9,6 +9,9 @@ class GameStats:
     # Start game in an active state.
     self.game_active = False
 
+    # High score.
+    self.high_score = 0
+
   def reset_stats(self):
     """Initialize stats that can chage in game."""
     self.ships_left = self.settings.ship_limit
--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -14,8 +14,10 @@ class Scoreboard:
     self.text_color = (30, 30, 30)
     self.font = pygame.font.SysFont(None, 48)
 
-    # Prepare initial score image.
+    # Prepare initial score images.
     self.prep_score()
+    self.prep_high_score()
+
 
   def prep_score(self):
     """Turn score into a rendered image."""
@@ -27,6 +29,24 @@ class Scoreboard:
     self.score_rect.right = self.screen_rect.right - 20
     self.score_rect.top = 20
 
+  def prep_high_score(self):
+    """Turn high score into a rendered image"""
+    high_score = self.stats.high_score
+    high_score_str = "{:,}".format(high_score)
+    self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+    # Center the high score at the top of the screen.
+    self.high_score_rect = self.high_score_image.get_rect()
+    self.high_score_rect.centerx = self.screen_rect.centerx
+    self.high_score_rect.top = self.score_rect.top
+
+  def check_high_score(self):
+    """Check to see if there is a new high score."""
+    if self.stats.score > self.stats.high_score:
+      self.stats.high_score = self.stats.score
+      self.prep_high_score()
+
   def show_score(self):
     """Draw score to screen."""
     self.screen.blit(self.score_image, self.score_rect)
+    self.screen.blit(self.high_score_image, self.high_score_rect)
