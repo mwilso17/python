c51ca9f16023466ba5f17f9d78b9b40b12307b0a
mwilso17 <michael.b.wilson2@gmail.com>
2025-03-23 17:29:47 -0400

    Added play button, start game in inactive state.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -8,6 +8,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -33,6 +34,9 @@ class AlienInvasion:
     self.aliens = pygame.sprite.Group()
 
     self._create_fleet()
+
+    # Make a play button.
+    self.play_button = Button(self, "Play")
 
     # Set background color.
     self.bg_color = (230, 230, 230)
@@ -195,6 +199,10 @@ class AlienInvasion:
     for bullet in self.bullets.sprites():
       bullet.draw_bullet()
     self.aliens.draw(self.screen)
+
+    # Draw play button if game is inactive.
+    if not self.stats.game_active:
+      self.play_button.draw_button()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
