22cde4e023b6224f80f449e3165c365d1600985a
mwilso17 <michael.b.wilson2@gmail.com>
2025-03-23 18:01:17 -0400

    bug fix, play button no longer clickable after game start.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -68,8 +68,22 @@ class AlienInvasion:
 
   def _check_play_button(self, mouse_pos):
     """Start a new game when player clicks Play."""
-    if self.play_button.rect.collidepoint(mouse_pos):
+    button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+    if button_clicked and not self.stats.game_active:
+      # Reset game stats.
+      self.stats.reset_stats()
       self.stats.game_active = True
+
+      # Get rid of remaining aliens and bullets.
+      self.aliens.empty()
+      self.bullets.empty()
+
+      # Create a new fleet and center the ship.
+      self._create_fleet()
+      self.ship.center_ship()
+
+      # Hide mouse cursor.
+      pygame.mouse.set_visible(False)
 
   def _check_keydown_events(self, event):
     """Respond to keypresses"""
@@ -178,6 +192,7 @@ class AlienInvasion:
       sleep(2)
     else:
       self.stats.game_active = False
+      pygame.mouse.set_visible(True)
 
   def _check_aliens_bottom(self):
     """Check if any aliens have reached the bottom of the screen."""
