b29e9d4c9d6ce0d18addcdd00fe678090e182c9e
mwilso17 <michael.b.wilson2@gmail.com>
2025-04-05 22:02:47 -0400

    added player speed and block off screen movement.

--- tank_you/settings.py
+++ tank_you/settings.py
@@ -5,3 +5,6 @@ class Settings:
     self.screen_width = 1000
     self.screen_height = 800
     self.bg_color = (100, 230, 100)
+
+    # Player tank speed.
+    self.tank_speed = .3
--- tank_you/tank.py
+++ tank_you/tank.py
@@ -6,6 +6,7 @@ class Tank:
   def __init__(self, ty_game):
     """Initialize tank and place it on the center left of the screen"""
     self.screen = ty_game.screen
+    self.settings = ty_game.settings
     self.screen_rect = ty_game.screen.get_rect()
 
     self.image = pygame.image.load('tank_you\images\player_tank.bmp')
@@ -13,16 +14,22 @@ class Tank:
 
     self.rect.midleft = self.screen_rect.midleft
 
+    # Store decimal value for vertical position.
+    self.y = float(self.rect.y)
+
     # Movement flag
     self.moving_up = False
     self.moving_down = False
 
   def update(self):
     """Update tank position based on movement flags."""
-    if self.moving_up:
-      self.rect.y -= 1
-    if self.moving_down:
-      self.rect.y += 1
+    if self.moving_up and self.rect.y > 0:
+      self.y -= self.settings.tank_speed
+    if self.moving_down and (self.rect.y + 50) < self.screen_rect.bottom:
+      self.y += self.settings.tank_speed
+
+    # Update rect object from self.y.
+    self.rect.y = self.y
 
 
   def blitme(self):
