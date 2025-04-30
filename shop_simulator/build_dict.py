c04f3637b089c7579779a9b3c69c6b465e4ee364
mwilso17 <michael.b.wilson2@gmail.com>
2025-04-30 18:48:23 -0400

    added quantity to inventory.txt and build_dict.py

--- shop_simulator/build_dict.py
+++ shop_simulator/build_dict.py
@@ -8,19 +8,22 @@ def build_inventory(inventory_file, divider = ","):
     with inventory_file as inventory:
       for item in inventory:
         item = item.strip()
-        if divider in item:
-          key, value = item.split(divider, 1)
-          inventory_dict[key.strip()] = value.strip()
+        product_info = item.split(',')
+        if len(product_info) == 3:
+          product = product_info[0].strip()
+          price = product_info[1].strip()
+          quantity = product_info[2].strip()
+          inventory_dict[product] = [price, quantity]
   except FileNotFoundError:
     print("File not found")
   return inventory_dict
 
 
-
+# Delete below when not testing. Will be used in the main cash register program.
 current_inventory = build_inventory(inventory_file)
 
 if current_inventory:
   for key, value in current_inventory.items():
-    print(f"{key}: {value}")
+    print(f"{key}: {value[0]} and {value[1]}")
 else:
   print("No data found")
