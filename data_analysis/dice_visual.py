b8f9aaf6f310e617cb722b403472b458f266091b
mwilso17 <michael.b.wilson2@gmail.com>
2025-05-20 04:25:00 -0400

    added bar graph.

--- data_analysis/dice_visual.py
+++ data_analysis/dice_visual.py
@@ -1,10 +1,28 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from dice import Die
 
 die = Die()
 
 results = []
-for roll_num in range(10):
+for roll_num in range(100):
   result = die.roll()
   results.append(result)
 
-print(results)
+# print(results)
+
+frequencies = []
+for value in range(1, die.num_sides+1):
+  frequency = results.count(value)
+  frequencies.append(frequency)
+
+# print(frequencies)
+
+x_values = list(range(1 + die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {"title" : "Result"}
+y_axis_config = {"title" : "Frequency of Results"}
+layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({"data" : data, "layout": layout})
