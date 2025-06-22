d5c923a2092fcdd810f4e2e24fb2da75d8d643c3
mwilso17 <michael.b.wilson2@gmail.com>
2025-06-22 03:39:23 -0400

    added .txt copy (free use) of Frankenstein by Mary Shelly. Created def to handle word count.

--- reading_analysis/text_analyzer.py
+++ reading_analysis/text_analyzer.py
@@ -1,9 +1,16 @@
-text_string = "This is a string of text."
+import string
 
-def get_word_count(text):
-  split_text = text.split()
-  word_count = len(split_text)
-  print(f"There are {word_count} words in this text.")
+text_file = open("reading_analysis\\frankenstein.txt", encoding="utf-8")
 
+def get_word_count(filename):
+    try:
+      with text_file as file:
+        text_data = file.read()
+        content = text_data.translate(str.maketrans('', '', string.punctuation)).lower()
+        word_list = content.split()
+        word_count = len(word_list)
+        print(f"There are approximately {word_count} words in this text.")
+    except FileNotFoundError:
+      print(f"Error: File '{filename}' not found.")
 
-get_word_count(text_string)
+get_word_count(text_file)
