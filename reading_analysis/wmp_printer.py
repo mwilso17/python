485052f7b9a5ef0717c218821e584c1297e0bbd0
mwilso17 <michael.b.wilson2@gmail.com>
2025-06-22 04:41:51 -0400

    restored punctuation and capitalization. Added User WMP input options.

--- reading_analysis/wmp_printer.py
+++ reading_analysis/wmp_printer.py
@@ -1,10 +1,10 @@
 # This file takes a text, splits its words into a list, and then prints the words
 # in that list at whatever speed the user wants (200 or 300 words per minute currently).
 
-# TODO: get rid of lower case only. Allow users to put in their own reading speed (input your reading speed, divide 60 by that number?).
-# allow document text to retain punctuation. exit loop on key press. pause/resume on key press. Allow user to increase or decrease speed 
-# on keypress or during pause.
-import string
+# TODO: Allow users to put in their own reading speed (input your reading speed, divide 60 by that number?).
+# exit loop on key press. pause/resume on key press. Allow user to increase or decrease speed 
+# on keypress or during pause. Validation for user input. Speed limits for user input (ie: someone says they
+# read at 1400 words per minute, ask them if they are sure.)
 import time
 
 text_file = open("reading_analysis\\shorter_sample.txt", encoding="utf-8")
@@ -13,7 +13,7 @@ def split_text(filename):
     try:
       with text_file as file:
         text_data = file.read()
-        content = text_data.translate(str.maketrans('', '', string.punctuation)).lower()
+        content = text_data.translate(str.maketrans('', ''))
         word_list = content.split()
         return word_list
     except FileNotFoundError:
@@ -25,6 +25,10 @@ words_in_file = split_text(text_file)
 two_hundred_wmp = .3
 three_hundred_wmp = .2
 
+user_wmp = input("Please enter your reading speed in words per minute: ")
+user_wmp_int = int(user_wmp)
+converted_wmp = 60/user_wmp_int
+
 for word in words_in_file:
    print(word)
-   time.sleep(two_hundred_wmp)
+   time.sleep(converted_wmp)
