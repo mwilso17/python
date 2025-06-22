9791dd5115bf01cdb2b1a31830a688d6d6edfaa3
mwilso17 <michael.b.wilson2@gmail.com>
2025-06-22 04:23:39 -0400

    wrote short story, implemented reading speed concept.

---
+++ reading_analysis/wmp_printer.py
@@ -0,0 +1,26 @@
+# This file takes a text, splits its words into a list, and then prints the words
+# in that list at whatever speed the user wants (200 or 300 words per minute currently).
+import string
+import time
+
+text_file = open("reading_analysis\\shorter_sample.txt", encoding="utf-8")
+
+def split_text(filename):
+    try:
+      with text_file as file:
+        text_data = file.read()
+        content = text_data.translate(str.maketrans('', '', string.punctuation)).lower()
+        word_list = content.split()
+        return word_list
+    except FileNotFoundError:
+      print(f"Error: File '{filename}' not found.")
+
+
+words_in_file = split_text(text_file)
+
+two_hundred_wmp = .3
+three_hundred_wmp = .2
+
+for word in words_in_file:
+   print(word)
+   time.sleep(two_hundred_wmp)
