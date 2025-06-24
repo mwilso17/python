9241e8746cc8417860b8761a0f3566a84b20371d
mwilso17 <michael.b.wilson2@gmail.com>
2025-06-24 04:35:40 -0400

    start pop up for speed reader/experiment with tkinter.

---
+++ reading_analysis/gui.py
@@ -0,0 +1,12 @@
+import tkinter
+import tkinter.messagebox
+
+
+root = tkinter.Tk()
+root.withdraw()  
+
+
+tkinter.messagebox.showinfo("Speed Reader", "Welcome to the speed reader!")
+
+
+root.destroy()
